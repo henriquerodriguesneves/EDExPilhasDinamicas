be3581e89bbc85925ebe603df00c1490f01b4444
rickl <rickl@192.168.0.201>
2021-09-17 00:16:40 -0300

    Segundo commit

--- src/controller/historicoController.java
+++ src/controller/historicoController.java
@@ -1,10 +1,50 @@
 package controller;
 
-import br.com.leandrocolevati.pilhastring;
-
+import br.com.leandrocolevati.pilhastring.Pilha;
 
 public class historicoController {
 	
+	public historicoController(){
+	super();
+	}
+	
+	public void insereEndereco(Pilha p, String site){
+		boolean valido = validaSite(site);
+		if (valido){
+			p.push(site);
+		} else {
+			System.err.println("Site inválido");
+		}
+	}
+	
+	public void removeEndereco(Pilha p) throws Exception {
+		if (!p.isEmpty()) {
+			String removido = p.pop();
+			System.out.println("Site: " + removido + " removido do histórico");
+			
+		} else {
+			System.err.println("Não há sistes no histórico");
+		}
+	}
+	
+	public void consultaUltimo(Pilha p) throws Exception {
+		if (!p.isEmpty()){
+			String top = p.top();
+			System.out.println("Último site visitado: " + top);
+		} else {
+			System.err.println("Não há sites no histórico");
+		}
+	}
+
+	private boolean validaSite(String site) {
+		String[] vetSite = site.split("\\.");
+		if (vetSite[0].equals("http://www") &&
+				vetSite.length > 2) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 	
 	
 	
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,6 +1,7 @@
 package view;
 
 import javax.swing.JOptionPane;
+import br.com.leandrocolevati.pilhastring.Pilha;
 
 import controller.historicoController;
 
@@ -9,13 +10,45 @@ public class Principal {
 	
 	public static void main(String[] args) {
 		
-		String novoEndereco;
-			
-		historicoController hCont = new historicoController();
+		historicoController histCont = new historicoController();
 		
-		novoEndereco = JOptionPane.showInputDialog(null, "Digite um endereço de e-mail");
+		Pilha historico = new Pilha();
 		
-		JOptionPane.showMessageDialog(null, novoEndereco);
+		int opc = 0;
+		while (opc != 9){
+			StringBuffer buffer = new StringBuffer();
+			buffer.append("Digite uma opção:\n");
+			buffer.append("1 - Inserir um site no histórico de navegação\n");
+			buffer.append("2 - Excluir um site do histórico de navegação\n");
+			buffer.append("3 - Consultar último site visitado\n");
+			buffer.append("9 - Sair\n");
+			String opcStr = JOptionPane.showInputDialog(null, buffer.toString());
+			opc = Integer.parseInt(opcStr);
+			switch (opc) {
+			case 1:
+				String site = JOptionPane.showInputDialog(null, "Digite um site");
+				histCont.insereEndereco(historico, site);
+				break;
+			case 2:
+				try {
+					histCont.removeEndereco(historico);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
+			case 3:
+				try {
+					histCont.consultaUltimo(historico);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
+			default:
+				break;
+			}
+		}
+		
+		
 		
 	}
 
